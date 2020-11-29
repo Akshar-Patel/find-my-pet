60434299f5097123c1719cfc416dc9060d518101
Akshar Patel <akshar26@gmail.com>
2020-11-29 20:09:16 +0530

    fix: Add name, location and description in Detail screen

--- lib/dog_breed_detail_arguments.dart
+++ lib/dog_breed_detail_arguments.dart
@@ -1,6 +1,8 @@
 class DogBreedDetailArguments {
   final String name;
   final String image;
+  final String description;
+  final String location;
 
-  DogBreedDetailArguments(this.name, this.image);
+  DogBreedDetailArguments(this.name, this.image, this.description, this.location);
 }
