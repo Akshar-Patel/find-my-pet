9b4a290e7f9a8a3854479f4201ee56fa46fac87f
Akshar Patel <akshar26@gmail.com>
2020-11-29 12:20:36 +0530

    feat: Fetch API data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'package:find_my_pet/api/api_client.dart';
+import 'package:find_my_pet/bloc/dog_breed_bloc.dart';
+import 'package:find_my_pet/bloc/dog_breed_event.dart';
+import 'package:find_my_pet/bloc/dog_breed_state.dart';
+import 'package:find_my_pet/repository/dog_breed_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(FindMyPetApp());
@@ -8,7 +14,7 @@ class FindMyPetApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Find My Pet',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -29,18 +35,36 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Find My Pet"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+      body: MultiBlocProvider(
+        providers:[
+          BlocProvider<DogBreedBloc>(
+            create: (context) {
+              return DogBreedBloc(
+                dogBreedRepository: DogBreedRepository(),
+              )..add(LoadDogBreeds());
+            },
+          )
+        ],
+        child: BlocBuilder<DogBreedBloc, DogBreedState>(
+          builder: (context, state){
+            if(state is DogBreedLoadSuccess){
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(state.dogBreeds.first.image),
+                  ],
+                ),
+              );
+            }else {
+              return Container();
+            }
+          }
         ),
       ),
     );
